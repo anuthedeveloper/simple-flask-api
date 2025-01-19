174932d36b3cdf0a2ae77f2c2f466e04c49d3355
aanu1234 <tunsabaze01@gmail.com>
2025-01-19 18:51:36 +0100

    Updated config file

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///testapidb.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fufu2019')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 
 
